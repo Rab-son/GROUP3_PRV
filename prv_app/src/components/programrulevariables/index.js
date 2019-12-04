a99c6e95a3ce5bab6001cd87511886a57dea3aa7
RabsonSayendaBsc-78-15 <rabsonsayendajnr@gmail.com>
2019-12-04 06:32:47 -0800

    First commit by RabsonSayenda

--- prv_app/src/components/programrulevariables/index.js
+++ prv_app/src/components/programrulevariables/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import {Grid, Paper} from '@material-ui/core'
 
 export default props => 
-    <div>
-        Program Rule variables
-    </div>
+    <Grid container sm>
+        <Grid item sm>
+            <Paper style={{padding : 50, marginTop : 10, marginRight : 20 }}>
+            Program Rule Variable Pane
+            </Paper>
+        </Grid>
+        <Grid item sm>
+            <Paper style={{padding : 50, marginTop : 10, marginRight : 20 }}>
+            Create Program Rule Variable Pane
+            </Paper>
+        </Grid>
+    </Grid>

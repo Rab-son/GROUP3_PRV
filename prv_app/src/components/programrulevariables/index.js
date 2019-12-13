75e1f994f00064d1cf5b2ee5544d16a6811e0017
piethans <bsc-92-16@cc.ac.mw>
2019-12-13 22:37:57 +0200

    css commit by solomon satoh

--- prv_app/src/components/programrulevariables/index.js
+++ prv_app/src/components/programrulevariables/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import {Grid, Paper} from '@material-ui/core'
+ 
+ const styles = {
+            Paper:{
+                padding:20,
+                marginTop:5,
+                marginRight:10,
+                marginLeft:10,
+                hieght:500,
+                overflowY:'auto'
+            }
+        }
 
 export default props => 
     <Grid container sm>
         <Grid item sm>
-            <Paper style={{padding : 50, marginTop : 10, marginRight : 20 }}>
+            <Paper style={styles.Paper}>
             Program Rule Variable Pane
             </Paper>
         </Grid>
         <Grid item sm>
-            <Paper style={{padding : 50, marginTop : 10, marginRight : 20 }}>
+            <Paper style={styles.Paper}>
             Create Program Rule Variable Pane
             </Paper>
         </Grid>

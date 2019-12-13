d18238014bdd07eae4fb181a445a9c6ed2df15a4
Rab-son <31789242+Rab-son@users.noreply.github.com>
2019-12-13 05:28:06 -0800

    Created a dialogs folder and a create.js with a dialogbox in it and created a CreateDialog component in header.js

--- prv_app/src/components/layouts/header.js
+++ prv_app/src/components/layouts/header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CreateDialog from '../programrulevariables/dialogs/create.js'
 // MUI imports
 import {AppBar, Toolbar, Typography} from '@material-ui/core'
 
@@ -12,5 +13,6 @@ export default props =>
             <Typography variant="h5" color="inherit" style={{flex: '1.5'}}>
                 PROGRAM RULE VARIABLE 
             </Typography>
+            <CreateDialog/>
         </Toolbar>
     </AppBar>  

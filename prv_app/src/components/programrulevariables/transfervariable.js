9bc66fa0ce060d4e0cbf051261a64800b235569f
Joseph Nkadana <josephnkadana5@gmail.com>
2019-12-27 19:56:17 -0800

    Updated transferlist.js in programrulevariable folder

--- prv_app/src/components/programrulevariables/transfervariable.js
+++ prv_app/src/components/programrulevariables/transfervariable.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
-//Material UI imports
-import {Button, Paper, Checkbox, List, ListItem, Grid } from '@material-ui/core';
+import React, { useState } from 'react'
+// mui imports
+import {Button, Paper, Checkbox, List, ListItem, Grid} from '@material-ui/core';
 import ArrowRight from '@material-ui/icons/ArrowForward';
 import ArrowLeft from '@material-ui/icons/ArrowBack';
 import Divider from '@material-ui/core/Divider';
 import CardHeader from '@material-ui/core/CardHeader';
 
-// Paper Styling
+// Paper Stylings
 const styles = {
-    Paper : { padding: 20, 
-              marginTop: 10,
-              marginBottom: 10,
-              marginRight: 10,
-              marginLeft: 10,
-              height: 300,
-              overflowY: "auto"}
-
+    Paper : {padding: 20,
+             marginTop: 10, 
+             marginBottom: 10, 
+             marginRight: 10, 
+             marginLeft: 10, 
+             height: 300, 
+             overflowY: 'auto'}
 }
-// Generating Program rule variables
+
+// Generating Program Rule Variables 
 function generatePRVs() {
     const PRVs = [];
-    for (let i = 1; i < 7; i++){
-        PRVs.push ({name : 'Data Element ' + i, box: 0, selected: false});
+    for (let i = 1; i< 7; i++) {
+        PRVs.push ({ name :'Program Rule Variable ' +i, box: 0, selected: false});
     }
     return PRVs;
 }
@@ -32,11 +32,6 @@ export default function ViewLayout() {
     function generateMarkup(PRVs) {
         return (
             <Paper style= {styles.Paper}> 
-                <CardHeader
-                    style= {styles.cardHeader}
-                /> 
-                   <h3 align="center">Data Elements</h3>           
-                <Divider/>
                 <List>
                     {PRVs.map(PRV =>
                      <ListItem>
@@ -50,26 +45,26 @@ export default function ViewLayout() {
             </Paper>
         )
     }
-    // reducing the data elements once from left pane once transfered to the right pane
+
     const [leftside, rightside] = PRVs.reduce((acc, cur) => {
         cur.box === 0 ? acc[0].push(cur) : acc[1].push(cur);
         return acc;
     }, 
     [[], []]
     );
-    // Changing box states
+
     function handleCheckboxChange(PRV) {
         const newPRVs = [...PRVs];
         const index = PRVs.findIndex(i => i === PRV);
         newPRVs[index].selected = !newPRVs[index].selected;
         setPRVs(newPRVs);
     }
-    // moving variable to the right pane of the transfer list
+
     function moveRight(){
         const newPRVs = PRVs.map(PRV => ({...PRV, box: PRV.selected ? 1: PRV.box}));
         setPRVs(newPRVs);
     }
-    // moving variable to the left pane of the transfer list
+    // moving variables
     function moveLeft(){
         const newPRVs = PRVs.map(PRV => ({...PRV, box: PRV.selected ? 0: PRV.box}));
         setPRVs(newPRVs);
@@ -78,6 +73,11 @@ export default function ViewLayout() {
         <div>
             <Grid container>
                 <Grid item xs={5}>
+                    <CardHeader
+                        style= {styles.cardHeader}
+                    /> 
+                    <h3 align="center">Data Elements</h3>           
+                    <Divider/>
                     {generateMarkup(leftside)}
                 </Grid>
                 <Grid item xs={2} container direction = "column" justify="center">
@@ -93,6 +93,11 @@ export default function ViewLayout() {
                  onClick={moveLeft}><ArrowLeft/></Button>  
                 </Grid>
                 <Grid item xs={5}>
+                    <CardHeader
+                        style= {styles.cardHeader}
+                    /> 
+                    <h3 align="center">Program Rule Variables</h3>           
+                    <Divider/>
                     {generateMarkup(rightside)} 
                 </Grid>
             </Grid>
